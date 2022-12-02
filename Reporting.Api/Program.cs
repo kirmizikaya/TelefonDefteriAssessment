f0d3581c86871c21aaf4615a2421425d500d1902
mehmet kırmızıkaya <mehmetkirmizikaya46@gmail.com>
2022-12-02 18:07:06 +0300

    fiexd namespace error

--- Reporting.Api/Program.cs
+++ Reporting.Api/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Shared.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
